10c0ff7f27c860ad25f51f56d75004c052b5a697
murshidazher <murshidazher@yahoo.com>
2020-06-11 19:23:29 +0530

    chore(webpack): add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./lib/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000
+  },
+  mode: "development"
+};
